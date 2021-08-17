45e1b28e543e2563311ddf4afae6ca50b585e332
pascaline <mbida.pascaline@gmail.com>
2021-08-17 06:14:02 +0200

    After ArtistController + ArtistSeeder+ routes

---
+++ app/Models/Artist.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Artist extends Model
+{
+    /**
+     * The attribute that are mass assignable
+     * @var array
+     */
+    use HasFactory;
+    protected $fillable=['firstname','lastname'];
+    /**
+     * the table associated with the model
+     * @var string
+     */
+    protected $table='artists';
+    /**
+     * indicates if the model should be timestamped
+     * @var bool
+     */
+    public $timestamps=false;
+}
