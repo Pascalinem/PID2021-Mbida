5b3c45a7de92eac73e3b684e1a62a1da62f0a017
pascaline <mbida.pascaline@gmail.com>
2021-08-22 10:52:05 +0200

    Controller Representation +templates +routes

--- resources/views/representation/index.blade.php
+++ resources/views/representation/index.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.app')
 
-@section('title', 'Liste des spectacles')
+@section('title', 'Liste des représentations')
 
 @section('content')
     <h1>Liste des {{ $resource }}</h1>
 
     <ul>
-    @foreach($shows as $show)
-    <li>
-        <a href="{{ route('show_show', $show->id) }}">{{ $show->title }}</a>
-        @if($show->bookable)
-        <span>{{ $show->price }} €</span>
+    @foreach($representations as $representation)
+    <li>{{ $representation->show->title }}
+        @if($representation->location)
+        - <span>{{ $representation->location->designation }}</span>
         @endif
+        - <datetime>{{ substr($representation->when,0,-3) }}</datetime>
     </li>
     @endforeach
     </ul>
