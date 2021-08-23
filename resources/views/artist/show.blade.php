a28608766cdd53e8cceef52f92fe373b1ae3f66a
pascaline <mbida.pascaline@gmail.com>
2021-08-23 12:27:13 +0200

    Update templates Artist & Type

--- resources/views/artist/show.blade.php
+++ resources/views/artist/show.blade.php
@@ -3,5 +3,11 @@
 @section('title','Fiche d\'un artiste')
 @section('content')
     <h1> {{ $artist->firstname }} {{ $artist->lastname }}</h1>
+    <h5>Liste des types de l'artiste<h5>
+       <ul> 
+        @foreach ($artist->types as $type)
+            <li>{{ $type->type }}</li>
+        @endforeach
+       </ul>
     <nav> <a href="{{ route('artist_index') }}">Retour à l'index</a> </nav>
 @endsection
--- resources/views/type/show.blade.php
+++ resources/views/type/show.blade.php
@@ -4,6 +4,12 @@
 @section('content')
 
   <p>  {{ $type->id }} {{ $type->type }}</p>
+  <h5>Liste d'artiste avec cette fonction</h5>
+  <ol>
+  @foreach ($type->artists as $artist)
+     <li>{{ $artist->firstname }} {{ $artist->lastname }}</li> 
+  @endforeach
+  </ol>
   <nav> <a href="{{ route('type_index') }}"> Retour à la liste </a>  </nav>
     
 @endsection
