a27d224045ad17a0c7a8297dc48d0e92d9d14e21
pascaline <mbida.pascaline@gmail.com>
2021-08-30 16:44:30 +0200

    update template index.blade location

--- resources/views/location/index.blade.php
+++ resources/views/location/index.blade.php
@@ -3,7 +3,7 @@
 @section('title','Liste des lieux de spectacle')
 
 @section('content')
-<h1> Liste des {{ resource }}</h1>
+<h1> Liste des lieux</h1>
 
 @foreach ($locations as $location)
     <p>{{ $location->id }} {{ $location->slug}} {{ $location->designation }} {{ $location->address }} {{ $location->locality_id }} {{ $location->website}} {{ $location->phone }}   </p>
